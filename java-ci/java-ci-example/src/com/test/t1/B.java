ce62c9ff299a074f28a322d28d6aefadde7dc2c1
林彦君 <josh.lin@pisofttech.com>
2017-07-12 13:20:29 +0800

    java-ci

---
+++ java-ci/java-ci-example/src/com/test/t1/B.java
@@ -0,0 +1,11 @@
+package com.test.t1;
+
+import java.io.Serializable;
+
+public class B implements Cloneable  {
+    private String someState;
+    private static final Log log = LogFactory.getLog();
+    
+    public void f() {
+    }
+}
---
+++ java-ci/java-ci-example/src/com/test/t1/LogFactory.java
@@ -0,0 +1,7 @@
+package com.test.t1;
+
+public class LogFactory {
+	static Log getLog() {
+		return new Log();
+	}
+}
